d5b12896cab21d8ab3d7935181d0a45cedadb6e3
Olivier Ory <mail@normegil.be>
2017-10-26 11:04:55 +0200

    Init streamutils

---
+++ stream.go
@@ -0,0 +1,13 @@
+package streamutils
+
+import (
+	"bytes"
+	"io"
+)
+
+// ToBytes convert an io.Reader into a byte array
+func ToBytes(stream io.Reader) []byte {
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(stream)
+	return buf.Bytes()
+}
